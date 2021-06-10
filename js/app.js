af39182596211753f5a33f49cbc948e106825084
Gerald Bivens <geraldbivens@gmail.com>
2021-06-10 16:13:40 -0600

    Add fonts and update styles.

--- js/app.js
+++ js/app.js
@@ -14,7 +14,9 @@ users.forEach(person => {
   const website = document.createElement('p');
   website.classList.add('website');
   if (person.website === null) website.classList.add('hide');
-  website.innerHTML = `<a href="https://${person.website}/" target="_blank" rel="noreferrer noopener">${person.website}</a>`;
+  website.innerHTML = `<a href="https://${person.website}/" target="_blank" rel="noreferrer noopener">
+    ${person.website}
+  </a>`;
 
   const contactInfoList = document.createElement('ul');
   contactInfoList.classList.add('contact-info-list');
@@ -22,26 +24,26 @@ users.forEach(person => {
   const email = document.createElement('li');
   if (person.email === null) email.classList.add('hide');
   email.innerHTML = `<a href="mailto: ${person.email}">
-            <img src="/assets/email.png" width="20" height="20" title="Email" />
-          </a>`;
+    <img src="/assets/graphics/email.png" width="20" height="20" title="Email" />
+  </a>`;
 
   const linkedin = document.createElement('li');
   if (person.linkedin === null) linkedin.classList.add('hide');
   linkedin.innerHTML = `<a href="${person.linkedin}" target="_blank" rel="noreferrer noopener">
-              <img src="/assets/linkedin.png" width="20" height="20" title="LinkedIn" />
-            </a>`;
+    <img src="/assets/graphics/linkedin.png" width="20" height="20" title="LinkedIn" />
+  </a>`;
 
   const github = document.createElement('li');
   if (person.github === null) github.classList.add('hide');
   github.innerHTML = `<a href="${person.github}" target="_blank" rel="noreferrer noopener">
-              <img src="/assets/github.png" width="20" height="20" title="GitHub" />
-            </a>`;
+    <img src="/assets/graphics/github.png" width="20" height="20" title="GitHub" />
+  </a>`;
 
   const medium = document.createElement('li');
   if (person.medium === null) medium.classList.add('hide');
   medium.innerHTML = `<a href="${person.medium}" target="_blank" rel="noreferrer noopener">
-              <img src="/assets/medium.png" width="20" height="20" title="Medium" />
-            </a>`;
+    <img src="/assets/graphics/medium.png" width="20" height="20" title="Medium" />
+  </a>`;
 
   contactInfoList.append(email, linkedin, github, medium);
 
